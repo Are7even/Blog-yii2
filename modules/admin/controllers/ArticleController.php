9740faa0d2b853963f91639dff46087c582cc092
Klimenko Evhen <jeka.klumenko@gmail.com>
2020-04-03 20:05:47 +0300

    ArticleController (Back to view after save image)

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -128,17 +128,20 @@ class ArticleController extends Controller
         throw new NotFoundHttpException('The requested page does not exist.');
     }
 
-    public function actionSetImage($id){
+    public function actionSetImage($id)
+    {
         $model = new ImageUpload;
-        if (Yii::$app->request->isPost)
-        {
+        if (Yii::$app->request->isPost) {
             $article = $this->findModel($id);
-            $file = UploadedFile::getInstance($model,'image');
+            $file = UploadedFile::getInstance($model, 'image');
 
-            $article->saveImage($model->uploadFile($file,$article->image));
+            if ($article->saveImage($model->uploadFile($file, $article->image))) ;
+            {
+                return $this->redirect(['view','id'=>$article->id]);
+            }
         }
 
-        return $this->render('image',['model'=>$model]);
+        return $this->render('image', ['model' => $model]);
     }
 
 }
